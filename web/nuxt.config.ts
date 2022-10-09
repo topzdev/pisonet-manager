79c5975a86d40ce9496d3fc0b4389a6f8cbe7524
christianwebdev200 <christian@webdev200.com>
2022-10-09 18:39:29 +0800

    project init

---
+++ web/nuxt.config.ts
@@ -0,0 +1,13 @@
+import { defineNuxtConfig } from "nuxt";
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  typescript: {
+    shim: false,
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+
+  tailwindcss: {
+    // Options
+  },
+});
